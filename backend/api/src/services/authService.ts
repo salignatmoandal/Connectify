3a8d2cd75eb9971464e02316b9f039b3a2f7c26c
Mawen Salignat-Moandal <mwnslgt@gmail.com>
2024-11-03 15:53:40 +0100

    AuthService

--- backend/api/src/services/authService.ts
+++ backend/api/src/services/authService.ts
@@ -1,1 +1,28 @@
 import { Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/extension";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'; 
+
+
+const prisma = new PrismaClient();
+
+export const registerUser = async (email: string, password:string) => {
+    const hashedPassword = await bcrypt.hash(password, 10); 
+    return await prisma.user.create({
+        data: {
+            email,
+            password : hashedPassword,
+            name, 
+        }
+    })
+}
+
+export const loginUser = async (email: string, password: string) => {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (user && await bcrypt.compare(password, user.password)) {
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, { expiresIn: '1d' });
+      return token;
+    } else {
+      throw new Error('Invalid credentials');
+    }
+  };
